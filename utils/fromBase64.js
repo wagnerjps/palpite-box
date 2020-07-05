ce4b01bbe28a465998b678691e4f751d21d8ebbc
Wagner Silva <wagnerjps@gmail.com>
2020-07-05 01:29:26 -0300

    Correção componente fromBase64

--- utils/fromBase64.js
+++ utils/fromBase64.js
@@ -1,8 +1,4 @@
-import React from 'react'
-
-const fromBase64 = value => {
+export const fromBase64 = value => {
     const buff = Buffer.from(value, 'base64')
     return buff.toString('ascii')
 }
-
-export default fromBase64
